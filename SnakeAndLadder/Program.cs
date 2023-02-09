8b1a481c68fa12917d1b26e542adc5ca9a873df4
preeti <pritich0896@gmail.com>
2023-02-09 20:34:24 +0530

    [Preeti]Checking Random Dies Output

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,6 +11,12 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Program");
+            int position = 0;
+            Console.WriteLine("Start Positon is : " + position);
+            Random random = new Random();
+            int dicethrown = random.Next(1, 7);
+            Console.WriteLine("\nDies Output is :" + dicethrown);
+
             Console.ReadLine();
         }
     }
