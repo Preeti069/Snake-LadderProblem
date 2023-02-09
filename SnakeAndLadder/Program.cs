9782ab282a66d1205d13e5d56cc30da26670e3b2
preeti <pritich0896@gmail.com>
2023-02-09 23:37:11 +0530

    [Preeti]Checking the position At snake or Ladder or No play

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,12 +12,39 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake And Ladder Program");
+
+            const int FirstPosition = 0;
             int position = 0;
-            Console.WriteLine("Start Positon is : " + position);
+            Console.WriteLine("Start Positon is : " + FirstPosition);
             Random random = new Random();
             int dicethrown = random.Next(1, 7);
             Console.WriteLine("\nDies Output is :" + dicethrown);
 
+            Random random2 = new Random();
+            int options = random.Next(1, 4);
+            if (options == 1)
+            {
+                Console.WriteLine("Player got no play");
+                dicethrown = 0;
+                position = dicethrown;
+                Console.WriteLine("The player position is: " + position);
+            }
+            else if (options == 2)
+            {
+                Console.WriteLine("Player got ladder");
+                position = (position + dicethrown);
+                Console.WriteLine("The player position is: " + position);
+            }
+            else
+            {
+                Console.WriteLine("Player got snake");
+                position = (position - dicethrown);
+                if (position < 0)
+                {
+                    position = FirstPosition;
+                }
+                Console.WriteLine("The player position is: " + position);
+            }
             Console.ReadLine();
         }
     }
